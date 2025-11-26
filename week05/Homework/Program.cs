847c161f9919b6f26b2e4e5a150f9ff4e0feedcb
jsbowen79 <jsbowen79@outlook.com>
2025-11-26 10:49:50 -0700

    Inheritance complete

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment test = new Assignment("Joseph Bowen", "How to take a test");
+
+        Console.WriteLine(test.GetSummary());
+
+        MathAssignment mathHomework = new MathAssignment("Joseph", "Algebra 2", "6.3", "1-100");
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        WritingAssignment paper = new WritingAssignment("Joseph Bowen", "English 101", "10 Tips for Getting an A on that Test");
+        Console.WriteLine(paper.GetWritingInformation()); 
     }
 }
